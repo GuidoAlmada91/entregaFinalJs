54da51337c476e86ba0d51f3e306955a89e4c816
Guido Almada <guido.almada.91@gmail.com>
2024-02-28 12:49:18 -0300

    Agregando Filtro

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 const cargarApiCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(respuesta => respuesta.json())
-        .then(data => mostrarPaises(data))
+        .then(data => {
+            console.log(data)
+            mostrarPaises(data);
+
+            const buscador = document.getElementById('buscador');
+            
+            buscador.addEventListener('input', () => {
+                const buscarpais = buscador.value;
+                mostrarPaises(data, buscarpais);
+            });
+        })
         .catch(error => {
             console.error('Error al obtener datos de la API:', error);
-        
         });
         
 }
 
-const mostrarPaises = paises => {
-    console.log(paises)
+const mostrarPaises = (paises, filtro = '') => {
     const container = document.getElementById('paises');
     container.innerHTML = '';
 
@@ -18,18 +26,24 @@ const mostrarPaises = paises => {
     const continenteSeleccionado = filtroContinente.value;
 
     const paisesFiltrados = paises.filter(pais => {
-        if (continenteSeleccionado === 'todos') {
-            return true;
-        } else{
-            return pais.region === continenteSeleccionado;
-        }
-    })
+        const cumpleFiltroContinente = (continenteSeleccionado === 'todos') || (pais.region === continenteSeleccionado);
+        const nombrePais = pais.name.common || '';  // Manejar casos en los que el nombre del país es undefined
+        const cumpleFiltroBusqueda = nombrePais.toLowerCase().includes(filtro.toLowerCase());
+
+        return cumpleFiltroContinente && cumpleFiltroBusqueda;
+    });
 
     paisesFiltrados.forEach(pais => {
         const paisElement = getPais(pais);
-        container.appendChild(paisElement)
+        container.appendChild(paisElement);
     });
 }
+const buscador = document.getElementById('buscador');
+buscador.addEventListener('input', () => {
+    const buscarpais = buscador.value;
+    mostrarPaises(data, buscarpais);
+});
+
 
 const mostrarDetalles = (nombre, poblacion, region, capital, languages) => {
     const languagesArray = Object.values(languages).join(', ');
@@ -60,7 +74,7 @@ const getPais = (pais) => {
     container.appendChild(button);
 
     return container
-    
+
 }
 
 const filtroContinente = document.getElementById('filtroContinente');
